f7e3768303417473cfba06cd32cde267830ebe9a
Phips Peter <pspeter333@gmail.com>
2021-02-15 08:16:12 -0800

    Add TypeScript
    Add support for TypeScript.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * The Airtable base API URL.
+ * Used as the base for constructing URL objects.
+ */
+export const AIRTABLE_API_URL = "https://api.airtable.com";
+/**
+ * The Airtable API version for the client.
+ */
+export const AIRTABLE_API_VERSION = "v0";
