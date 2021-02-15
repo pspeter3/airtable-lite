a9b0473834827c6e870ca1393fbc91b769183881
Phips Peter <pspeter333@gmail.com>
2021-02-15 08:43:31 -0800

    Add AirtableError
    Add support for custom errors.

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,20 @@ export const AIRTABLE_API_URL = "https://api.airtable.com";
  * The Airtable API version for the client.
  */
 export const AIRTABLE_API_VERSION = "v0";
+
+/**
+ * Subclass of error to represent Airtable API specific errors.
+ */
+export class AirtableError extends Error {
+    readonly type: string;
+
+    /**
+     * Creates an Airtable API error.
+     * @param type The Airtable Error type.
+     * @param message The Airtable Error message.
+     */
+    constructor(type: string, message: string) {
+        super(message);
+        this.type = type;
+    }
+}
