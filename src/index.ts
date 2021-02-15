6ef90a17932a94f281ce15f6940fe7f4008cb8e3
Phips Peter <pspeter333@gmail.com>
2021-02-15 08:49:31 -0800

    Create basic client
    Create a simple Airtable client.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,27 @@ export const AIRTABLE_API_URL = "https://api.airtable.com";
 export const AIRTABLE_API_VERSION = "v0";
 
 /**
+ * Airtable client for a single table within a base.
+ */
+export class Airtable {
+    private readonly _apiKey: string;
+    private readonly _base: string;
+    private readonly _table: string;
+
+    /**
+     * Creates a client for a table with Airtable.
+     * @param apiKey Your Airtable API Key.
+     * @param base The Airable Base ID.
+     * @param table The Airtable Table Name.
+     */
+    constructor(apiKey: string, base: string, table: string) {
+        this._apiKey = apiKey;
+        this._base = base;
+        this._table = table;
+    }
+}
+
+/**
  * Subclass of error to represent Airtable API specific errors.
  */
 export class AirtableError extends Error {
