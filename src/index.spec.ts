415d7dc2e86ce9e306f0b94bc70951b854853aa3
Phips Peter <pspeter3@users.noreply.github.com>
2021-02-15 10:20:10 -0800

    Create basic CRUD client (#4)
    * Create basic client
    
    Create a simple Airtable client.
    
    * Add Airtable test harness
    
    Make it easy to test with fetch mocks.
    
    * Add tests for internals
    
    Test the internals of the client.
    
    * Add status to AirtableError
    
    This should help with status codes.
    
    * Add create
    
    Also add better tests for find.
    
    * Make create single record
    
    Will add bulkCreate to create many.
    
    * Add bulkCreate
    
    Add a way to create a bulk list of records.
    
    * Add update
    
    Add support for updating a record.
    
    * Add deletion
    
    Add the ability to delete records.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,10 @@
-import { AirtableError, AIRTABLE_API_URL, AIRTABLE_API_VERSION } from "./index";
+import fetchMock from "jest-fetch-mock";
+import {
+    Airtable,
+    AirtableError,
+    AIRTABLE_API_URL,
+    AIRTABLE_API_VERSION,
+} from "./index";
 
 describe("Constants", () => {
     it("should have Airtable API URL", () => {
@@ -12,16 +18,274 @@ describe("Constants", () => {
 
 describe("AirtableError", () => {
     it("should have the correct properties", () => {
+        const status = 401;
         const type = "AUTHENTICATION_REQUIRED";
         const message = "Authentication required";
-        const err = new AirtableError(type, message);
+        const err = new AirtableError(status, type, message);
+        expect(err.status).toBe(status);
         expect(err.type).toBe(type);
         expect(err.message).toBe(message);
     });
 
     it("should have the correct inheritance", () => {
-        const err = new AirtableError("", "");
+        const err = new AirtableError(500, "", "");
         expect(err).toBeInstanceOf(AirtableError);
         expect(err).toBeInstanceOf(Error);
+    });
+});
+
+describe("Airtable", () => {
+    beforeAll(() => {
+        fetchMock.enableMocks();
+    });
+
+    it("should support a simple constructor", () => {
+        expect(() => new Airtable("", "", "")).not.toThrow();
+    });
+
+    describe("internals", () => {
+        it("should construct URLs correctly", async () => {
+            const base = "base";
+            const table = "name";
+            const id = "id";
+            const client = new Airtable("", base, table);
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.url).toBe(
+                    [
+                        AIRTABLE_API_URL,
+                        AIRTABLE_API_VERSION,
+                        base,
+                        table,
+                        id,
+                    ].join("/")
+                );
+                return JSON.stringify({
+                    id,
+                    createdTime: new Date(),
+                    fields: {},
+                });
+            });
+            await client.find(id);
+        });
+
+        it("should add the correct headers", async () => {
+            const key = "key";
+            const id = "id";
+            const client = new Airtable(key, "base", "table");
+            fetchMock.mockResponseOnce(async (req) => {
+                const headers = req.headers;
+                expect(headers.get("Authorization")).toBe(`Bearer ${key}`);
+                expect(headers.get("Content-Type")).toBe("application/json");
+                return JSON.stringify({
+                    id,
+                    createdTime: new Date(),
+                    fields: {},
+                });
+            });
+            await client.find(id);
+        });
+
+        it("should handle errors", async () => {
+            const status = 401;
+            const type = "AUTHENTICATION_REQUIRED";
+            const message = "Authentication required";
+            const client = new Airtable("key", "base", "table");
+            fetchMock.mockResponseOnce(
+                JSON.stringify({
+                    error: {
+                        type,
+                        message,
+                    },
+                }),
+                { status }
+            );
+            await expect(() => client.find("id")).rejects.toThrowError(
+                new AirtableError(status, type, message)
+            );
+        });
+    });
+
+    describe("find", () => {
+        it("should return a record", async () => {
+            const id = "id";
+            const record = {
+                id,
+                createdTime: new Date().toString(),
+                fields: {},
+            };
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.url).toMatch(new RegExp(`/${id}$`));
+                expect(req.method).toBe("GET");
+                return JSON.stringify(record);
+            });
+            const result = await new Airtable("", "", "").find(id);
+            expect(result).toEqual(record);
+        });
+    });
+
+    describe("create", () => {
+        it("should create a record", async () => {
+            const fields = {};
+            const data = {
+                id: "0",
+                createdTime: new Date().toString(),
+                fields,
+            };
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.method).toBe("POST");
+                const body = await req.json();
+                expect(body.fields).toEqual(fields);
+                return JSON.stringify(data);
+            });
+            const record = await new Airtable("", "", "").create(fields);
+            expect(record).toEqual(data);
+        });
+
+        it("should create a record with typecast", async () => {
+            const fields = {};
+            const data = {
+                id: "0",
+                createdTime: new Date().toString(),
+                fields,
+            };
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.method).toBe("POST");
+                const body = await req.json();
+                expect(body.fields).toEqual(fields);
+                expect(body.typecast).toBe(true);
+                return JSON.stringify(data);
+            });
+            const record = await new Airtable("", "", "").create(fields, true);
+            expect(record).toEqual(data);
+        });
+    });
+
+    describe("update", () => {
+        it("should update a record", async () => {
+            const id = "0";
+            const fields = {};
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.url).toMatch(new RegExp(`/${id}$`));
+                expect(req.method).toBe("PATCH");
+                const body = await req.json();
+                expect(body.fields).toEqual(fields);
+                return JSON.stringify({
+                    id,
+                    createdTime: new Date(),
+                    fields: body.fields,
+                });
+            });
+            const record = await new Airtable("", "", "").update(id, fields);
+            expect(record).toMatchObject({ id, fields });
+        });
+
+        it("should update a record with typecast", async () => {
+            const id = "0";
+            const fields = {};
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.url).toMatch(new RegExp(`/${id}$`));
+                expect(req.method).toBe("PATCH");
+                const body = await req.json();
+                expect(body.fields).toEqual(fields);
+                expect(body.typecast).toBe(true);
+                return JSON.stringify({
+                    id,
+                    createdTime: new Date(),
+                    fields: body.fields,
+                });
+            });
+            const record = await new Airtable("", "", "").update(
+                id,
+                fields,
+                false,
+                true
+            );
+            expect(record).toMatchObject({ id, fields });
+        });
+
+        it("should update a record destructively", async () => {
+            const id = "0";
+            const fields = {};
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.url).toMatch(new RegExp(`/${id}$`));
+                expect(req.method).toBe("PUT");
+                const body = await req.json();
+                expect(body.fields).toEqual(fields);
+                return JSON.stringify({
+                    id,
+                    createdTime: new Date(),
+                    fields: body.fields,
+                });
+            });
+            const record = await new Airtable("", "", "").update(
+                id,
+                fields,
+                true
+            );
+            expect(record).toMatchObject({ id, fields });
+        });
+    });
+
+    describe("delete", () => {
+        it("should delete a record", async () => {
+            const id = "id";
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.url).toMatch(new RegExp(`/${id}$`));
+                expect(req.method).toBe("DELETE");
+                return JSON.stringify({ id, deleted: true });
+            });
+            const result = await new Airtable("", "", "").delete(id);
+            expect(result).toEqual({ id, deleted: true });
+        });
+    });
+
+    describe("bulkCreate", () => {
+        it("should create a list of records", async () => {
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.method).toBe("POST");
+                const body: {
+                    records: { fields: Record<string, unknown> }[];
+                } = await req.json();
+                const records = body.records.map(({ fields }, index) => ({
+                    id: index.toString(),
+                    createdTime: new Date(),
+                    fields,
+                }));
+                return JSON.stringify({ records });
+            });
+            const { records } = await new Airtable("", "", "").bulkCreate([
+                {},
+                {},
+            ]);
+            expect(records).toMatchObject([
+                { id: "0", fields: {} },
+                { id: "1", fields: {} },
+            ]);
+        });
+
+        it("should create a list of records with typecast", async () => {
+            fetchMock.mockResponseOnce(async (req) => {
+                expect(req.method).toBe("POST");
+                const body: {
+                    records: { fields: Record<string, unknown> }[];
+                    typecast: boolean;
+                } = await req.json();
+                expect(body.typecast).toBe(true);
+                const records = body.records.map(({ fields }, index) => ({
+                    id: index.toString(),
+                    createdTime: new Date(),
+                    fields,
+                }));
+                return JSON.stringify({ records });
+            });
+            const { records } = await new Airtable("", "", "").bulkCreate(
+                [{}, {}],
+                true
+            );
+            expect(records).toMatchObject([
+                { id: "0", fields: {} },
+                { id: "1", fields: {} },
+            ]);
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,188 @@ export const AIRTABLE_API_URL = "https://api.airtable.com";
 export const AIRTABLE_API_VERSION = "v0";
 
 /**
+ * Airtable client for a single table within a base.
+ */
+export class Airtable<T> {
+    private readonly _apiKey: string;
+    private readonly _base: string;
+    private readonly _table: string;
+
+    /**
+     * Creates a client for a table with Airtable.
+     * @param apiKey Your Airtable API Key.
+     * @param base The Airable Base ID.
+     * @param table The Airtable Table Name.
+     */
+    constructor(apiKey: string, base: string, table: string) {
+        this._apiKey = apiKey;
+        this._base = base;
+        this._table = table;
+    }
+
+    /**
+     * Finds a record by ID.
+     * @param id The Record ID.
+     */
+    async find(id: string): Promise<AirtableRecord<T>> {
+        return this._dispatch(
+            new Request(this._createURL(id).toString(), {
+                headers: this._createHeaders(),
+            })
+        );
+    }
+
+    /**
+     * Creates a record.
+     * @param fields The fields of the record to create.
+     * @param typecast Whether to typecast the record.
+     */
+    async create(fields: T, typecast?: true): Promise<AirtableRecord<T>> {
+        return this._dispatch(
+            new Request(this._createURL().toString(), {
+                method: "POST",
+                headers: this._createHeaders(),
+                body: JSON.stringify({ fields, typecast }),
+            })
+        );
+    }
+
+    /**
+     * Updates a single record.
+     * @param id The record ID to update.
+     * @param fields The fields to update.
+     * @param destructive Whether to clear unspecified fields.
+     * @param typecast Whether to typecast the record.
+     */
+    async update(
+        id: string,
+        fields: Partial<T>,
+        destructive = false,
+        typecast?: true
+    ): Promise<AirtableRecord<T>> {
+        return this._dispatch(
+            new Request(this._createURL(id).toString(), {
+                method: destructive ? "PUT" : "PATCH",
+                headers: this._createHeaders(),
+                body: JSON.stringify({ fields, typecast }),
+            })
+        );
+    }
+
+    /**
+     * Deletes a record.
+     * @param id The record ID to delete.
+     */
+    async delete(id: string): Promise<AirtableDeletion> {
+        return this._dispatch(
+            new Request(this._createURL(id).toString(), {
+                method: "DELETE",
+                headers: this._createHeaders(false),
+            })
+        );
+    }
+
+    /**
+     * Bulk creates a list of record.
+     * @param records The records to create.
+     * @param typecast Whether to typecast the record.
+     */
+    async bulkCreate(
+        records: ReadonlyArray<T>,
+        typecast?: true
+    ): Promise<AirtableRecordsResponse<AirtableRecord<T>>> {
+        return this._dispatch(
+            new Request(this._createURL().toString(), {
+                method: "POST",
+                headers: this._createHeaders(),
+                body: JSON.stringify({
+                    records: records.map((fields) => ({ fields })),
+                    typecast,
+                }),
+            })
+        );
+    }
+
+    private async _dispatch<R>(req: Request): Promise<R> {
+        const res = await fetch(req);
+        const data = await res.json();
+        if (!res.ok) {
+            const { error } = data as AirtableErrorResponse;
+            throw new AirtableError(res.status, error.type, error.message);
+        }
+        return data;
+    }
+
+    private _createURL(...path: string[]): URL {
+        return new URL(
+            [AIRTABLE_API_VERSION, this._base, this._table, ...path].join("/"),
+            AIRTABLE_API_URL
+        );
+    }
+
+    private _createHeaders(json = true): Headers {
+        const headers = new Headers();
+        headers.set("Authorization", `Bearer ${this._apiKey}`);
+        if (json) {
+            headers.set("Content-Type", "application/json");
+        }
+        return headers;
+    }
+}
+
+/**
  * Subclass of error to represent Airtable API specific errors.
  */
 export class AirtableError extends Error {
+    /**
+     * The HTTP Status Code.
+     */
+    readonly status: number;
+    /**
+     * The Airtable API Error Type.
+     */
     readonly type: string;
 
     /**
      * Creates an Airtable API error.
-     * @param type The Airtable Error type.
-     * @param message The Airtable Error message.
+     * @param status The HTTP Status Code.
+     * @param type The Airtable Error Type.
+     * @param message The Airtable Error Message.
      */
-    constructor(type: string, message: string) {
+    constructor(status: number, type: string, message: string) {
         super(message);
+        this.status = status;
         this.type = type;
     }
+}
+
+/**
+ * Generic interface for an Airtable Record.
+ */
+export interface AirtableRecord<T> {
+    readonly id: string;
+    readonly createdTime: string;
+    readonly fields: T;
+}
+
+/**
+ * Generic interface for an object containing Airtable Records.
+ */
+export interface AirtableRecordsResponse<T> {
+    readonly records: ReadonlyArray<T>;
+}
+
+/**
+ * Interface for Airtable deletion response.
+ */
+export interface AirtableDeletion {
+    readonly id: string;
+    readonly deleted: boolean;
+}
+
+interface AirtableErrorResponse {
+    readonly error: {
+        readonly type: string;
+        readonly message: string;
+    };
 }
